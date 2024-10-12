17d876d6926e97ac6ecdb1346ba87e912335624d
LuisAlonsoAndradeLopez <caffeinated340@gmail.com>
2024-10-12 00:32:07 -0600

    Adding basic structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DahAutobiography
-Uno de los proyectos que pide el Juan Luis para la materia Desarrollo de Sistemas Web.
+Este es uno de los proyectazos que pide el maestro Juan Luis para poder acreditar la materia Desarrollo de Sistemas Web... madre mía no quiero llevarme a repite Proyecto Guiado.
---
+++ script.js
@@ -0,0 +1,21 @@
+var sidebar = document.getElementsByClassName('sidebar')[0];
+
+function showSidebarButtonOnClick() {
+    sidebar.classList.add('move-sidebar-right');
+}
+
+function sidebarCloseButtonOnClick() {
+    sidebar.classList.add('move-sidebar-left');
+}
+
+function whoAmIButtonOnClick() {
+
+}
+
+function habilitiesButtonOnClick() {
+
+}
+
+function profilesButtonOnClick() {
+
+}
