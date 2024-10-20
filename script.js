a96caf712f822ddcc6fc0347f5ff187f4e2eb0d3
LuisAlonsoAndradeLopez <caffeinated340@gmail.com>
2024-10-19 21:19:39 -0600

    Sidebar done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var sidebar = document.getElementsByClassName('sidebar')[0];
+var whoAmIContent = document.getElementsByClassName('who-am-i-content')[0];
+var projectsContent = document.getElementsByClassName('projects-content')[0];
+var profilesContent = document.getElementsByClassName('profiles-content')[0];
 
 function sidebarCloseButtonOnClick() {
-    sidebar.style.backgroundColor = "lightblue";
     sidebar.style.display = "none";
 }
 
@@ -10,13 +12,19 @@ function showSidebarButtonOnClick() {
 }
 
 function whoAmIButtonOnClick() {
-
+    whoAmIContent.style.display = "flex";
+    projectsContent.style.display = "none";
+    profilesContent.style.display = "none";
 }
 
-function habilitiesButtonOnClick() {
-
+function projectsButtonOnClick() {
+    whoAmIContent.style.display = "none";
+    projectsContent.style.display = "flex";
+    profilesContent.style.display = "none";
 }
 
 function profilesButtonOnClick() {
-
+    whoAmIContent.style.display = "none";
+    projectsContent.style.display = "none";
+    profilesContent.style.display = "flex";
 }
